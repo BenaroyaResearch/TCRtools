bd76f9bd2eba3319b12fdf6079faf18e9f90065b
mjdufort <matt.dufort@gmail.com>
2019-10-09 15:18:25 -0700

    Update documentation for BLAST TCR search functions

--- R/write_TCR_junction_fasta.R
+++ R/write_TCR_junction_fasta.R
@@ -4,7 +4,7 @@
 #' protein sequences. This allows identification of known TCRs from published sequences.
 #' @param tcrs data frame, the TCrs to be included. Must include all the columns specified in \code{cols_for_name} and \code{junction_col}.
 #' @param filename character string, the path and file name to output. Defaults to "tcr_junctions.fasta"
-#' @param pos_control character string containing a positive control TCR sequence that will be prepended to the fasta file. Used to ensure that BLAST search parameters are working properly. Can be set to NULL to not include positive control.
+#' @param pos_control character string containing a positive control TCR sequence that will be prepended to the fasta file. Used to ensure that BLAST search parameters are working properly. Can be set to NULL to not include positive control. The default value should give a perfect match to protein accession 1OGA_D.
 #' @param sample_col character, the name of the column in \code{tcrs} to be included in the fasta query name. Separate from \code{cols_for_name} to enable easier editing.
 #' @param cols_for_name character vector, the columns in \code{tcrs} to be included in the fasta query name. Values will be concatenated with "_" as separator to generate the query name. To use alignment results with \code{read_BLAST_TCR_align_hits} with length thresholding, include the junction at the end of the query name.
 #' @param junction_col character string, the name of the column in \code{tcrs} to use for the CDR3 junction sequence. Must be included in \code{tcrs}.
@@ -18,6 +18,13 @@
 #'   cols_for_name=c(sample_col, "cln_count", "v_gene", "j_gene", "junction"),
 #'   junction_col="junction",
 #'   unique_only=TRUE)
+#' @details
+#' This function outputs a file designed to be used for a BLAST query on
+#' https://blast.ncbi.nlmh.nih.gov. On that site, use protein blast (blastp), under "Choose File",
+#' upload the file that is output by this function. Make sure that the non-redundant protein sequences
+#' database (nr) is selected, set organism to "Homo sapiens (taxid:9606)", and under Algorith parameters
+#' set Expect threshold to 1000 (to account for the length of the sequences being queried). If you
+#' included the default pos_control, you should see it match to protein accession 1OGA_D.
 write_TCR_junction_fasta <-
   function(tcrs, filename="tcr_junctions.fasta",
            pos_control=">flu_1_TRBV_CAGAGSQGNLIF CAGAGSQGNLIF",
